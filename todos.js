952b8e3ffee63954f83b1581dc184352fb528623
Dongha Kim <ghost9087@gmail.com>
2015-03-12 18:33:18 +0900

    add phpstorm ignores 12th march code for class

--- todos.js
+++ todos.js
@@ -1,1 +1,35 @@
 //start...
+
+function getDom(id){
+    return document.getElementById(id);
+}
+var todoStringField = getDom('todoString');
+todoStringField.addEventListener('keyup', function(event){
+    if(event.keyCode === 13){
+        var currentValue = todoStringField.value;
+        todoStringField.value = '';
+
+        var todoList = getDom('todoList');
+        todoList.innerHTML += '<li>\
+        <button class=\"delete\">×</button>\
+        <input type=\"checkbox\" class=\"toggle-checked\">\
+        <span class=\"text\">'+currentValue+'</span>\
+        </li>';
+    }
+});
+
+
+
+//var todoForm = getDom('todoForm');
+//
+//todoForm.addEventListener('submit', function(ev){
+//    ev.preventDefault();
+//    console.log(todoStringField, todoStringField.value);
+//});
+
+
+
+
+
+
+
